eec5fa8d73a271284c1a5074580f9283e7360f28
Arnost55 <99980693+Arnost55@users.noreply.github.com>
2025-02-10 19:34:11 +0100

    hello

--- main.bat
+++ main.bat
@@ -12,6 +12,9 @@ start cmd.exe
 start cmd.exe
 start cmd.exe
 start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
 
 cd %USERPROFILE%/Desktop
 start https://www.howtogeek.com/173478/10-important-computer-security-practices-you-should-follow/#:~:text=Basic%20Computer%20Security%3A%20How%20to%20Protect%20Yourself%20from,...%207%20Don%27t%20Trust%20Any%20Popup%20Notifications%20
